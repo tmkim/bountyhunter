e2d9dab5efb8e68da2e298ada51745dc2bb08b71
tmkim <taeminkim13@gmail.com>
2025-06-24 17:56:52 -0400

    updates to deck test script

--- scripts/db_test_deck.py
+++ scripts/db_test_deck.py
@@ -3,7 +3,38 @@ from setup.db_connect import connect_psql
 import questionary
 import sqlalchemy
 from sqlalchemy import select, and_, or_, join, tuple_
-from setup.db.models import OnePieceDeck
+from setup.db.models import OnePieceDeck, OnePieceCard, OnePieceDeckCard
+
+def deck_summ(df_deck):
+    deck_id = df_deck['id'].iloc[0] 
+
+    with connect_psql().connect() as conn:
+        stmt_deck_cards = select(OnePieceDeckCard).where(
+            OnePieceDeckCard.deck_id == deck_id
+        )
+        df_deck_cards = pd.read_sql(stmt_deck_cards, conn)
+        df_deck_cards.rename(columns={
+            'card_id': 'id'
+        }, inplace=True)
+
+        stmt_card_list = select(OnePieceCard).where(
+            OnePieceCard.id.in_(df_deck_cards['id'].tolist())
+        )
+        df_card_list = pd.read_sql(stmt_card_list, conn)
+
+    # print(df_card_list)
+    # print(df_deck_cards)
+
+    df_deck_summ = df_card_list.merge(df_deck_cards, on=["id"])
+    df_deck_summ = df_deck_summ[['id', 'name', 'market_price', 'quantity']]
+    df_deck_summ['card_price'] = round(df_deck_summ['market_price'] * df_deck_summ['quantity'], 2)
+    print(df_deck_summ)
+
+    # df_card_list['card_price'] = round(df_card_list['market_price'] * df_card_list['quantity'], 2)
+    # df_summ = df_card_list[['card_id', 'name', 'market_price', 'quantity', 'card_price']].sort_values('card_id')
+
+    # print(f"Total Price: ${round((df_card_list['market_price'] * df_card_list['quantity']).sum(),2)}")
+    # print(df_summ)
 
 user = input("Enter username: ")
 
@@ -26,4 +57,4 @@ except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
 if 'df_deck' in locals() and not df_deck.empty:
-    print(df_deck)
+    deck_summ(df_deck)
