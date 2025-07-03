dbe4cdbd627d1891e87ab7bc78d0965c3270f71b
tmkim <taeminkim13@gmail.com>
2025-07-02 21:34:43 -0400

    update card search

--- scripts/card_search.py
+++ scripts/card_search.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import questionary
 from setup.db_connect import connect_psql
 
 database = connect_psql()
@@ -16,15 +17,27 @@ def search_df(df, query, columns, delimiter="_"):
 
 df = pd.read_sql('one_piece_card', database)
 
-query = input('Enter card name: ')
-search_name = df[df['name'].str.contains(query, case=False, na=False)]
-print(f'{search_name.shape} cards found')
+# query = input('Enter card name: ')
+# search_name = df[df['name'].str.contains(query, case=False, na=False)]
+# print(f'{search_name.shape} cards found')
 
-query = input('Enter card color: ')
-search_color = search_name[search_name['color'].str.contains(query, case=False, na=False)]
+# query = input('Enter card color: ')
+# search_color = search_name[search_name['color'].str.contains(query, case=False, na=False)]
 
-card_results = search_color[['name', 'color', 'card_id', 'market_price']]
-print(card_results.head)
+# card_results = search_color[['name', 'color', 'card_id', 'market_price']]
+# print(card_results.head)
 
-filtered = search_df(df, "100_Luffy_pur", columns=['card_number', 'name', 'color'])
-print(filtered)
+query = input('Enter card query: ')
+filtered = search_df(df, query, columns=['card_id', 'name', 'color'])
+card_results = filtered[['name', 'color', 'card_id', 'market_price']]
+print(card_results)
+
+df_card_id = card_results.drop_duplicates(subset=['card_id'])
+card_id_list = df_card_id['card_id'].tolist()
+card_id_list = list(filter(None, card_id_list))
+print(card_id_list)
+
+card_select = questionary.select("Select a card: ", choices=card_id_list).ask()
+filtered_select = search_df(card_results, card_select, columns=['card_id'])
+filtered_select = filtered_select[['name', 'market_price']].sort_values(['market_price'])
+print(filtered_select)
