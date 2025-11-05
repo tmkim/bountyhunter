11bd1df658a5238453d9ba0844ff2f8ffa378b76
tmkim <taeminkim13@gmail.com>
2025-11-05 17:12:02 -0500

    update color themes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,24 +1,12 @@
 // frontend/tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-content: [
-  "./pages/**/*.{ts,tsx}",
-  "./components/**/*.{ts,tsx}",
-  "./app/**/*.{ts,tsx}",
-  "./src/**/*.{ts,tsx}",
-],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
 
-  theme: {
-    extend: {
-      colors: {
-        maya: "#60BFF5",
-        lapis: "#2E63A4",
-        brown: "#AF6528",
-        tangerine: "#FFCE00",
-        rosso: "#D70000",
-        black: "#000000",
-      },
-    },
-  },
   plugins: [require("tailwindcss-animate")],
 };
